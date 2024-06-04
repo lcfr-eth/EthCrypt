be736a5faddb613945551776b7852ab15ccb9ffd
lcfr <me@lcfr.io>
2024-06-03 22:05:20 -0400

    clap

---
+++ src/opts.rs
@@ -0,0 +1,43 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(
+    name = "eth-crypter", 
+    version = "1.0", 
+    about = "Encrypt/Decrypt text & files using ETH keypairs", 
+    long_about = None
+)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    Test,
+    Pubkey,
+    Encrypt {
+        #[arg(help = "Public key in hex format")]
+        public_key_hex: String,
+        #[arg(help = "Message to encrypt")]
+        message: String,
+    },
+    Decrypt {
+        #[arg(help = "Encrypted message in hex format")]
+        encrypted_hex: String,
+    },
+    EncryptFile {
+        #[arg(help = "Public key in hex format")]
+        public_key_hex: String,
+        #[arg(help = "Input file path")]
+        input_file: String,
+        #[arg(help = "Output file path")]
+        output_file: String,
+    },
+    DecryptFile {
+        #[arg(help = "Input file path")]
+        input_file: String,
+        #[arg(help = "Output file path")]
+        output_file: String,
+    },
+}
